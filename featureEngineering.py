a7351bb7eb73525e268e96f7bbbf54ae53d9a83a
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-04-25 16:55:03 +0100

    Finished making notes on feature engineering

--- featureEngineering.py
+++ featureEngineering.py
@@ -66,3 +66,118 @@ X_4["MSClass"] = X["MSSubClass"].str.split("_", n=1, expand=True)[0]
 
 #Counts are especially helpful for tree models, since these models don't 
 #have a natural way of aggregating information across many features at once.
+
+# Consider using unsupervised clustering and then using cluster number as
+# an additional column (but one-hot encode clusters rather than giving 1,2,3,...)
+# or even fit a different model to each cluster
+# Create cluster feature
+kmeans = KMeans(n_clusters=6)
+X["Cluster"] = kmeans.fit_predict(X)
+X["Cluster"] = X["Cluster"].astype("category")
+
+# === PCA ===
+# standardize data before applying PCA
+# Consider removing or constraining outliers, since they can have an undue influence on the results.
+
+# can be used for anomaly detection
+# Anomaly detection: Unusual variation, not apparent from the original features, will often show up 
+# in the low-variance components. These components could be highly informative in an anomaly or outlier detection task.
+
+# as well as decorrelation
+# Decorrelation: Some ML algorithms struggle with highly-correlated features. PCA transforms correlated features 
+# into uncorrelated components, which could be easier for your algorithm to work with.
+
+features = ["highway_mpg", "engine_size", "horsepower", "curb_weight"]
+
+X = df.copy()
+y = X.pop('price')
+X = X.loc[:, features]
+
+# Standardize
+X_scaled = (X - X.mean(axis=0)) / X.std(axis=0)
+
+from sklearn.decomposition import PCA
+
+# Create principal components
+pca = PCA()
+X_pca = pca.fit_transform(X_scaled)
+
+# Convert to dataframe
+component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+X_pca = pd.DataFrame(X_pca, columns=component_names)
+
+# and to show loadings
+loadings = pd.DataFrame(
+    pca.components_.T,  # transpose the matrix of loadings
+    columns=component_names,  # so the columns are the principal components
+    index=X.columns,  # and the rows are the original features
+)
+
+# Show dataframe sorted by PC3
+idx = X_pca["PC3"].sort_values(ascending=False).index
+cols = ["make", "body_style", "horsepower", "curb_weight"]
+df.loc[idx, cols]
+
+# Show outliers based on PCA components
+component = "PC1"
+
+idx = X_pca[component].sort_values(ascending=False).index
+df.loc[idx, ["SalePrice", "Neighborhood", "SaleCondition"] + features]
+
+# === Target encoding ===
+
+# simple mean encoding
+# out
+# index make 		price 	make_encoded
+#>> 1	alfa-romero	16500	15498.333333
+#>> 2	alfa-romero	16500	15498.333333
+#>> 3	audi		13950	17859.166667
+#>> 4	audi		17450	17859.166667
+
+# Smoothing: blend the in-category average with the overall average (protects against rare extremes)
+encoding = weight * in_category + (1 - weight) * overall
+# weight based on category frequency, e.g.:
+weight = n / (n + m)
+# where m is the smoothing factor
+
+# Target encoding is great for:
+# High-cardinality features: A feature with a large number of categories can be troublesome to encode: 
+# a one-hot encoding would generate too many features and alternatives, like a label encoding, might 
+# not be appropriate for that feature. A target encoding derives numbers for the categories using the 
+# feature's most important property: its relationship with the target.
+
+# Domain-motivated features: From prior experience, you might suspect that a categorical feature 
+# should be important even if it scored poorly with a feature metric. A target encoding can help reveal 
+# a feature's true informativeness.
+
+# 25% train split to train the target encoder
+X = df.copy()
+y = X.pop('Rating')
+
+X_encode = X.sample(frac=0.25)
+y_encode = y[X_encode.index]
+X_pretrain = X.drop(X_encode.index)
+y_train = y[X_pretrain.index]
+
+from category_encoders import MEstimateEncoder
+
+# Create the encoder instance. Choose m to control noise.
+encoder = MEstimateEncoder(cols=["Zipcode"], m=5.0)
+
+# Fit the encoder on the encoding split.
+encoder.fit(X_encode, y_encode)
+
+# Encode the Zipcode column to create the final training data
+X_train = encoder.transform(X_pretrain)
+
+# usually good idea to encode high cardinality cats. can use this to check
+df.select_dtypes(["object"]).nunique()
+
+# to prevent overfitting need to split before encoding
+# Encoding split
+X_encode = df.sample(frac=0.20, random_state=0)
+y_encode = X_encode.pop("SalePrice")
+
+# Training split
+X_pretrain = df.drop(X_encode.index)
+y_train = X_pretrain.pop("SalePrice")
