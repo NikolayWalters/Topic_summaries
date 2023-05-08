9577fa66971971b1b95e0dfa4eac54075e1c0761
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-05-08 17:47:24 +0100

    Finished CNN notes

--- CNN.py
+++ CNN.py
@@ -104,3 +104,67 @@ image_condense = tf.nn.pool(
 
 # padding='valid' = convolution window will stay entirely inside the input
 # padding='same' = pad with zeros
+
+# Example multi block conv net
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+
+    # First Convolutional Block
+    layers.Conv2D(filters=32, kernel_size=5, activation="relu", padding='same',
+                  # give the input dimensions in the first layer
+                  # [height, width, color channels(RGB)]
+                  input_shape=[128, 128, 3]),
+    layers.MaxPool2D(),
+
+    # Second Convolutional Block
+    layers.Conv2D(filters=64, kernel_size=3, activation="relu", padding='same'),
+    layers.MaxPool2D(),
+
+    # Third Convolutional Block
+    layers.Conv2D(filters=128, kernel_size=3, activation="relu", padding='same'),
+    layers.MaxPool2D(),
+
+    # Classifier Head
+    layers.Flatten(),
+    layers.Dense(units=6, activation="relu"),
+    layers.Dense(units=1, activation="sigmoid"),
+])
+model.summary()
+
+# compile and train
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(epsilon=0.01),
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy']
+)
+
+history = model.fit(
+    ds_train,
+    validation_data=ds_valid,
+    epochs=40,
+    verbose=0,
+)
+
+# Data augmentation
+model = keras.Sequential([
+    # Preprocessing
+    preprocessing.RandomFlip('horizontal'), # flip left-to-right
+    preprocessing.RandomContrast(0.5), # contrast change by up to 50%
+    # Base
+    pretrained_base,
+    # Head
+    layers.Flatten(),
+    layers.Dense(6, activation='relu'),
+    layers.Dense(1, activation='sigmoid'),
+])
+
+# some other options:
+preprocessing.RandomContrast(factor=0.5),
+preprocessing.RandomFlip(mode='horizontal'), # meaning, left-to-right
+preprocessing.RandomFlip(mode='vertical'), # meaning, top-to-bottom
+preprocessing.RandomWidth(factor=0.15), # horizontal stretch
+preprocessing.RandomRotation(factor=0.20),
+preprocessing.RandomTranslation(height_factor=0.1, width_factor=0.1),
+# all of the "factor" parameters indicate a percent-change
