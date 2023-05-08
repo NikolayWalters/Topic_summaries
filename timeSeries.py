f0aafae97b529f4d173dfefca8b69402f53f5aeb
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-05-08 19:09:31 +0100

    Started summarising Time Series course

---
+++ timeSeries.py
@@ -0,0 +1,104 @@
+"""
+Kagle Time Series course notes
+"""
+
+# easy way to convert from date to "time"
+df['Time'] = np.arange(len(tunnel.index))
+
+
+# fitting linear reg
+from sklearn.linear_model import LinearRegression
+
+# Training data
+X = df.loc[:, ['Time']]  # features
+y = df.loc[:, 'NumVehicles']  # target
+
+# Train the model
+model = LinearRegression()
+model.fit(X, y)
+
+# Store the fitted values as a time series with the same time index as
+# the training data
+y_pred = pd.Series(model.predict(X), index=X.index)
+
+
+# can use in-built pandas to create lag feature (i.e. previous day feature)
+df['Lag_1'] = df['NumVehicles'].shift(1)
+
+# since the first lag value is nan can drop it
+from sklearn.linear_model import LinearRegression
+
+X = df.loc[:, ['Lag_1']]
+X.dropna(inplace=True)  # drop missing values in the feature set
+y = df.loc[:, 'NumVehicles']  # create the target
+y, X = y.align(X, join='inner')  # drop corresponding values in target
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = pd.Series(model.predict(X), index=X.index)
+
+
+# rolling mean
+moving_average = tunnel.rolling(
+    window=365,       # 365-day window
+    center=True,      # puts the average at the center of the window
+    min_periods=183,  # choose about half the window size
+).mean()              # compute the mean (could also do median, std, min, max, ...)
+
+
+# better way to create lag
+from statsmodels.tsa.deterministic import DeterministicProcess
+
+dp = DeterministicProcess(
+    index=tunnel.index,  # dates from the training data
+    constant=True,       # dummy feature for the bias (y_intercept)
+    order=1,             # the time dummy (trend)
+    drop=True,           # drop terms if necessary to avoid collinearity
+)
+# `in_sample` creates features for the dates given in the `index` argument
+X = dp.in_sample()
+
+# out
+# Day			const	trend		
+# 2003-11-01	1.0		1.0
+# 2003-11-02	1.0		2.0
+# 2003-11-03	1.0		3.0
+# 2003-11-04	1.0		4.0
+
+
+# to predict
+X = dp.out_of_sample(steps=30)
+
+y_fore = pd.Series(model.predict(X), index=X.index)
+
+
+# seasonal plots
+# days within a week
+X["day"] = X.index.dayofweek  # the x-axis (freq)
+X["week"] = X.index.week  # the seasonal period (period)
+
+# days within a year
+X["dayofyear"] = X.index.dayofyear
+X["year"] = X.index.year
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(11, 6))
+seasonal_plot(X, y="NumVehicles", period="week", freq="day", ax=ax0)
+seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1);
+
+
+
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+
+fourier = CalendarFourier(freq="A", order=10)  # 10 sin/cos pairs for "A"nnual seasonality
+# can use "M" for monthly, etc
+
+dp = DeterministicProcess(
+    index=tunnel.index,
+    constant=True,               # dummy feature for bias (y-intercept)
+    order=1,                     # trend (order 1 means linear)
+    seasonal=True,               # weekly seasonality (indicators)
+    additional_terms=[fourier],  # annual seasonality (fourier)
+    drop=True,                   # drop terms to avoid collinearity
+)
+
+X = dp.in_sample()  # create features for dates in tunnel.index
