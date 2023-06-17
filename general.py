03ad04bd7e568611511362740abb1793252d5c80
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-06-17 19:13:03 +0100

    Added xgboost default params

--- general.py
+++ general.py
@@ -348,6 +348,12 @@ for j in range(n_comp):
 # if good accuracy check for data leaks
 
 # slap xgboost
+# NB defaults
+XGBClassifier(base_score=0.5, colsample_bylevel=1, colsample_bytree=1,
+       gamma=0, learning_rate=0.1, max_delta_step=0, max_depth=10,
+       min_child_weight=1, missing=None, n_estimators=100, nthread=-1,
+       objective='binary:logistic', reg_alpha=0, reg_lambda=1,
+       scale_pos_weight=1, seed=0, silent=True, subsample=1)
 import xgboost as xgb
 model = xgb.XGBClassifier()
 model.fit(X_train_sub, y_train)
